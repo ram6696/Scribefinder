def8a2c2025b6c22bba650493da03343b35cccae
ram6696 <c.sriram.619@gmail.com>
2018-10-29 22:29:31 +0530

    Splashscreen udjustments

--- app/src/main/java/com/ourapps/scribefinder/Intro.java
+++ app/src/main/java/com/ourapps/scribefinder/Intro.java
@@ -1,4 +1,5 @@
 package com.ourapps.scribefinder;
+
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -7,15 +8,12 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class Intro extends AppCompatActivity {
 
@@ -99,7 +97,7 @@ public class Intro extends AppCompatActivity {
 
     private void launchHomeScreen() {
         prefManager.setFirstTimeLaunch(false);
-        startActivity(new Intent(Intro.this, Login.class));
+        startActivity(new Intent(Intro.this, TypeOfUser.class));
         finish();
     }
 
